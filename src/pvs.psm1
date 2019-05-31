8faf96f6196aa439a16bd3553c659e72e05ceebb
Andi Rückauer <arueckauer@github.com>
2019-05-31 23:42:22 +0200

    Add functionality and to-do's

--- src/pvs.psm1
+++ src/pvs.psm1
@@ -13,17 +13,53 @@
   pvs 7.3
 #>
 
+#Requires -RunAsAdministrator
+
+# @todo Add configuration file with
+# - PHP versions
+# - Apache path
+# - Smylink path & name
+# - Directory(/ies) containing log files
+# @todo Parameter to just restart Apache
+# @todo Parameter to delete log files
 function pvs {
     [CmdletBinding()]
 
+    # Fetch version to switch to
     Param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)] [Alias("v")] [String]$version
     )
+
+    switch ($version) {
+        "7.0" { $phpPath = "C:\webserv\php-7.0.33-Win32-VC14-x64" }
+        "7.1" { $phpPath = "C:\webserv\php-7.1.29-Win32-VC14-x64" }
+        "7.2" { $phpPath = "C:\webserv\php-7.2.18-Win32-VC15-x64" }
+        "7.3" { $phpPath = "C:\webserv\php-7.3.5-Win32-VC15-x64" }
+        "7.4" { $phpPath = "C:\webserv\php-7.4-ts-windows-vc15-x64-r3322ae2" }
+        Default { throw "Provided PHP version $version is not supported" }
+    }
 
     Write-Host "PHP Version Switcher 1.0.0 by Andi Rückauer" -ForegroundColor Blue
     Write-Host ""
     Write-Host "Switching to version: $version..." -ForegroundColor DarkCyan
     Write-Host ""
+
+    # Remove SymbolicLink
+    # @todo Only delete, if it does exist
+    Write-Host "Removing SymbolicLink..." -ForegroundColor DarkCyan
+    Write-Host ""
+    (Get-Item C:\webserv\php\).Delete()
+
+    # Create new symlink
+    Write-Host "Creating SymbolicLink..." -ForegroundColor DarkCyan
+    Write-Host ""
+    New-Item -ItemType SymbolicLink -Path C:\webserv\ -Name php -Value $phpPath
+
+    # Restart Apache
+    # @todo If log files are to be deleted: Stop Apache service, delete log files, start Apache service
+    Write-Host "Restarting Apache..." -ForegroundColor DarkCyan
+    Write-Host ""
+    C:\webserv\httpd-2.4.39-win64-VC15\Apache24\bin\httpd.exe -k restart
 }
 
 Export-ModuleMember -Function pvs
